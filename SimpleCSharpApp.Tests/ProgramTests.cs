183f8cbea4f9205aa7b772efe7900bfc0feb2f61
KUNAL MAJI <kunalmaji@KUNALs-MacBook-Air.local>
2024-12-29 00:46:53 +0530

    Initial commit

---
+++ SimpleCSharpApp.Tests/ProgramTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace SimpleCSharpApp.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void TestConsoleOutput()
+        {
+            var output = "Hello, GitHub Actions!";
+            Assert.Equal("Hello, GitHub Actions!", output);
+        }
+    }
+}
---
+++ SimpleCSharpApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleCSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitHub Actions!");
+        }
+    }
+}
